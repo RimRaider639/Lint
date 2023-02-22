1179242eb106e58c307ef525ead7c9be2aea2dfb
Rajendra3049 <Rajendrapatel3049@gmail.com>
2023-02-22 12:51:46 +0530

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-import ProductsPage from "./pages/productsPage";
-
 function App() {
-  return (
-    <div>
-      <ProductsPage />
-    </div>
-  );
-=======
-
-function App() {
-  return <></>;
->>>>>>> 3ba635dcf2c1f6dd5f415cc289a7b8cc3f317673
+  return <div></div>;
 }
 
 export default App;
