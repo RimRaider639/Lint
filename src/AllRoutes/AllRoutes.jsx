f6a74d59a8fb6ff3250a2359b71d16c8c16cbf24
RimRaider639 <madhurimapoddar639@gmail.com>
2023-02-24 22:39:47 +0530

    day5

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -10,12 +10,9 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products/:path" element={<ProductsPage />} />
-<<<<<<< HEAD
       <Route path="/single" element={<SingleProductPage />} />
       <Route path="/cart" element={<Cart/>}/>
-=======
       <Route path="/product/:id" element={<SingleProductPage />} />
->>>>>>> 93d21bbbcbf487d1b5aeb78c36bf2ebe2b9b1458
     </Routes>
   );
 };
