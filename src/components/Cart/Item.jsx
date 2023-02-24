1f2bbf3f02a931ec903ee26648cd10d876891b76
RimRaider639 <madhurimapoddar639@gmail.com>
2023-02-24 21:50:50 +0530

    getting cart data, updating quanity, total price

--- src/components/Cart/Item.jsx
+++ src/components/Cart/Item.jsx
@@ -5,24 +5,30 @@ import { updateCartItem } from '../../redux/cart/cart.actions'
 import { useDispatch } from 'react-redux'
 
 const Item = ({cartItem}) => {
-    const {productID:item, count} = cartItem
+    const {productID:item, count, _id} = cartItem
     const dispatch = useDispatch()
     const [qtd, setQtd] = React.useState(count)
     React.useEffect(()=>{
-        dispatch(updateCartItem(item._id, qtd))
+        dispatch(updateCartItem(_id, qtd))
     }, [qtd])
   return (
-    <Flex minH="200px" gap="20px" border="1px solid black" p="1em 2em" bgColor={'white'}>
-        <Flex w="20%">
-            <Image src={item.image[0]} alt={item.product_name} w="80px" h="fit-content" fallbackSrc={fallbackSrc}/>
+    <Flex border="1px solid #e4e4e4" gap="25px" p="1em 2em" bgColor={'white'}>
+        <Flex w="15%" minH="150px" align={'center'}>
+            <Image src={item.image[0]} alt={item.product_name} w="100%" h="fit-content" fallbackSrc={fallbackSrc}/>
         </Flex>
-        <Flex flexGrow={1} justify={'space-between'} fontSize={'14px'}>
-            <Flex w="40%" direction={'column'}>
-                <Text  fontWeight={'700'}>{item.product_name}</Text>
-                <Text>PID: {item.pid}</Text>
-                <Text>₹{item.discounted_price}</Text>
+        <Flex flexGrow={1} justify={'space-between'} fontSize={'14px'} h="65%">
+            <Flex w="40%" direction={'column'} justify={'space-between'}>
+                <Flex direction={'column'}>
+                    <Text  fontWeight={'700'}>{item.product_name}</Text>
+                    <Text>PID: {item.pid}</Text>
+                    <Text>₹{item.discounted_price.toFixed(2)}</Text>
+                </Flex>
+                <Flex>
+                    <Text borderBottom={'1px solid #0076be'}  _hover={{borderBottom:'none'}} color='#0076be'>Edit</Text>
+                </Flex>
+                
             </Flex>
-            <Flex w="40%" direction={'column'} align={'flex-end'} gap="10px" fontWeight={'700'}>
+            <Flex w="40%"  h="65%" direction={'column'} align={'flex-end'} gap="10px" fontWeight={'700'}>
                 <Flex gap="10px" w="100px">
                     <Text>Qtd: </Text>
                     <Select borderRadius={0} size={'sm'} value={qtd} onChange={e=>setQtd(+e.target.value)}>
@@ -34,8 +40,8 @@ const Item = ({cartItem}) => {
                         <option value={6}>6</option>
                     </Select>
                 </Flex>
-                <Text>Item Total: ₹{item.discounted_price*count}</Text>
-                <Flex gap="10px" fontWeight={'400'} color='#0076be'>
+                <Text>Item Total: ₹{(item.discounted_price*qtd).toFixed(2)}</Text>
+                <Flex gap={"10px"} justify={'space-between'} fontWeight={'400'} color='#0076be'>
                     <Text borderBottom={'1px solid #0076be'}  _hover={{borderBottom:'none'}}>Remove</Text>
                     <Text borderBottom={'1px solid #0076be'}  _hover={{borderBottom:'none'}}>Move to Wish List</Text>
                 </Flex>
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCartItems } from '../redux/cart/cart.actions'
 import Item from '../components/Cart/Item'
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Text, Divider } from '@chakra-ui/react'
 
 const Cart = () => {
     const dispatch = useDispatch()
@@ -13,19 +13,42 @@ const Cart = () => {
     console.log(items)
   return (
     total.count && 
-    <Flex pt="140px" w="80%" m="auto" direction={'column'} border="2px solid black">
+    <Flex pt="140px" w="80%" m="auto" direction={'column'} fontFamily={'Arial'}>
         <Text fontSize={'18px'} color="#222222" mb="10px">My Bag</Text>
-        <Flex border="2px solid black" w="100%">
-            <Flex direction={'column'} p="1em" bgColor={'#f5f5f5'} border="2px solid black" flexGrow={1}>
+        <Flex w="100%"  gap="35px">
+            <Flex direction={'column'} borderRadius={'10px'} p="1em" bgColor={'#f5f5f5'} flexGrow={1}>
                 <Text fontSize={'18px'} color="#222222">Shipping by Belk</Text>
                 <Text color='#767676' fontSize={'14px'}>{total.items} items</Text>
                 <Flex direction={'column'}  gap="20px" mt="20px">
                     {items.map(item=><Item key={item._id} cartItem={item}/>)}
                 </Flex>   
             </Flex>
-            <Flex w="30%" border="2px solid black" h="400px">
-
+            <Flex direction={'column'} w="50%">
+                <Flex borderRadius={'10px'} w="100%" border="1px solid #e4e4e4" p="1em" direction={'column'} fontSize={'14px'}>
+                    <Text fontSize={'18px'} color="#222222" mb="10px">Order Summary</Text>
+                    <Flex gap="5px">
+                        <Text>Coupons & Belk Bucks </Text>
+                        <Text borderBottom={'1px solid #0076be'}  _hover={{borderBottom:'none'}} color='#0076be'> Add</Text>
+                    </Flex>
+                    
+                    <Divider color={'#e4e4e4'} m="10px 0"/>
+                    <Flex justify={'space-between'}>
+                        <Text>Subtotal (x{total.items} items)</Text>
+                        <Text>₹{total.price.toFixed(2)}</Text>
+                    </Flex>
+                    <Flex justify={'space-between'}>
+                        <Text>Est. Shipping (Belk)</Text>
+                        <Text>FREE</Text>
+                    </Flex>
+                    <Divider color={'#e4e4e4'} m="10px 0"/>
+                    <Flex justify={'space-between'}>
+                        <Text fontSize={'16px'} fontWeight={'700'}>Estimated Total</Text>
+                        <Text>₹{total.price.toFixed(2)}</Text>
+                    </Flex>
+                    <Text mt="40px">or 4 interest-free payments of <bold>$63.75</bold> with</Text>
+                </Flex>
             </Flex>
+            
     </Flex>
     </Flex>
   )
