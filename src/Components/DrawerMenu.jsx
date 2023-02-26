09e00dda4e0186ecf0f4dad198c24745bc20fe55
TejasBishnoi21 <tejas.vishnoi@gmail.com>
2023-02-26 21:10:05 +0530

    final_changes_done-admin-complete

--- src/Components/DrawerMenu.jsx
+++ src/Components/DrawerMenu.jsx
@@ -1,72 +1,94 @@
-import { HamburgerIcon } from '@chakra-ui/icons'
+import { HamburgerIcon } from "@chakra-ui/icons";
 import {
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    Button,
-    useDisclosure,
-    Flex,
-  } from '@chakra-ui/react'
-import { useRef } from 'react'
-import { useNavigate } from 'react-router-dom';
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button,
+  useDisclosure,
+  Flex,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import DarkModeButton from "./DarkModeButton";
 
-  const DrawerMenu = ()=>{
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = useRef()
-    const navigate = useNavigate()
-  
-    return (
-      <>
-        <Button ref={btnRef} display={{md:'none'}}
-        colorScheme='blue' onClick={onOpen}>
-          <HamburgerIcon/>
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement='right'
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerHeader>Menu</DrawerHeader>
-  
-            <DrawerBody>
-                <Flex direction={'column'} gap={10}>
-                    <Button onClick={()=>{
-                      navigate('/')
-                      onClose()
-                    }}>Home</Button>
-                    <Button onClick={()=>{
-                      navigate('/inventory')
-                      onClose()
-                    }}>Inventory</Button>
-                    <Button onClick={()=>{
-                      navigate('/add')
-                      onClose()
-                    }}>Add Products</Button>
-                    
-                    <Button onClick={()=>{
-                      navigate('/users')
-                      onClose()
-                    }}>Users</Button>
-                </Flex>
-            </DrawerBody>
-  
-            <DrawerFooter gap={10}>
-              <DarkModeButton/>
-              <Button colorScheme='blue'>Logout</Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </>
-    )
-  }
+const DrawerMenu = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+  const navigate = useNavigate();
 
-  export default DrawerMenu;
+  return (
+    <>
+      <Button
+        ref={btnRef}
+        display={{ md: "none" }}
+        colorScheme="blue"
+        onClick={onOpen}
+      >
+        <HamburgerIcon />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+
+          <DrawerBody>
+            <Flex direction={"column"} gap={10}>
+              <Button
+                onClick={() => {
+                  navigate("/");
+                  onClose();
+                }}
+              >
+                Home
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/inventory");
+                  onClose();
+                }}
+              >
+                Inventory
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/add");
+                  onClose();
+                }}
+              >
+                Add Products
+              </Button>
+
+              <Button
+                onClick={() => {
+                  navigate("/users");
+                  onClose();
+                }}
+              >
+                Users
+              </Button>
+            </Flex>
+          </DrawerBody>
+
+          <DrawerFooter gap={10}>
+            <DarkModeButton />
+            <Button colorScheme="blue">
+              <a href="https://lint-lovat.vercel.app/">Logout</a>
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default DrawerMenu;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,22 @@
-import { Box, Button, Flex, Image } from "@chakra-ui/react";
-
+import { Box, Button, Flex, Image, useDisclosure } from "@chakra-ui/react";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+} from '@chakra-ui/react'
 import brand from "../Images/brand_img.png";
 import DrawerMenu from "./DrawerMenu";
 import DarkModeButton from "./DarkModeButton";
 import AdminAccount from "./AdminAccount";
+import { useRef } from "react";
 
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = useRef();
+
   return (
     <Box boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px" p="6">
       <Flex justify="space-between" align="center">
@@ -23,7 +34,33 @@ const Navbar = () => {
             <a href="https://lint-lovat.vercel.app/">Main Website</a>
           </Button>
           <DarkModeButton />
-          <Button>Logout</Button>
+          <Button onClick={onOpen}>Logout</Button>
+          <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Logout
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You will be redirected to main website.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='red' onClick={onClose} ml={3}>
+                <a href="https://lint-lovat.vercel.app/">Logout</a>
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
 
           <AdminAccount />
         </Flex>
